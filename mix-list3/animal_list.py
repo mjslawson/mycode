e68174708ddeb55244cd452349710dbab8dde470
Matthew Lawson <mjslawson@gmail.com>
2018-10-21 19:15:22 -0400

    Python lists, the final showdown

---
+++ mix-list3/animal_list.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+animals = ['Fox', 'Fly', 'Ant', 'Bee', 'Cod', 'Cat', 'Dog', 'Yak', 'Cow', 'Hen', 'Koi', 'Hog', 'Jay', 'Kit']
+
+# Print list as string with spaces
+print(" ".join(animals))
