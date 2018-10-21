f952ca94ff1b3dc62c529ec8336548477c042c36
Matthew Lawson <mjslawson@gmail.com>
2018-10-21 15:50:46 -0400

    Adds new python inputs script

---
+++ lab_input/input_ip.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Written By: Matthew Lawson
+# Collect IP address information from a user, and display it back to them.
+    
+## Collect input from the user
+user_input = input('Please enter an IPv4 IP address:') # user_input stores the IPv4 ip address
+    
+## Print out the input collected from the user
+print("You told me the IPv4 address is:" + user_input)
+
+## Collect input from the user
+vendor_name = input('Please enter a vendor name:') # user_input stores the vendor name
+    
+## Print out the input collected from the user
+print("You told me the vendor name is:" + vendor_name)
