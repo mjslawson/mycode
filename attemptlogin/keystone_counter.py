653fea17f1e328cf03e2eb76564e2dff6a6cdf74
Matthew Lawson <mjslawson@gmail.com>
2018-11-10 18:31:23 -0500

    Parsing log files

---
+++ attemptlogin/keystone_counter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Parsing Log Files
+
+Author: mjslawson
+"""
+
+def main():
+    """ Main Program """
+    import re
+    loginfail = 0
+    loginsuccess = 0
+    ip_list = []
+    keystone_file = open('/home/student/mycode/attemptlogin/keystone.common.wsgi','r')
+    keystone_file_lines=keystone_file.readlines()
+    for i in range(len(keystone_file_lines)):
+        if "- - - - -] Authorization failed" in keystone_file_lines[i]:
+            loginfail += 1 # this is the same as loginfail = loginfail + 1
+            ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', keystone_file_lines[i])
+            ip_list.extend(ip)
+        elif " - - - - -] Loaded 2 encryption keys" in keystone_file_lines[i]:
+            loginsuccess += 1
+    print('The number of failed log in attempts is ' + str(loginfail))
+    print('Failed login attempts from: ' + str(ip_list))
+    print('The number of successful log in attempts is ' + str(loginsuccess))
+
+main()
